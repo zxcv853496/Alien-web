8d92c33674e7ce5ff5a8aa0e42af7711138de09d
zxcv853496 <zxcv853496@gmail.com>
2026-01-02 19:25:37 +0800

    fix: switch to HashRouter for GitHub Pages compatibility

--- src/main.jsx
+++ src/main.jsx
@@ -7,18 +7,18 @@ import CssBaseline from '@mui/material/CssBaseline';
 import theme from './theme';
 import './i18n';
 
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { HelmetProvider } from 'react-helmet-async';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <HelmetProvider>
-      <BrowserRouter>
+      <HashRouter>
         <ThemeProvider theme={theme}>
           <CssBaseline />
           <App />
         </ThemeProvider>
-      </BrowserRouter>
+      </HashRouter>
     </HelmetProvider>
   </React.StrictMode>,
 )
